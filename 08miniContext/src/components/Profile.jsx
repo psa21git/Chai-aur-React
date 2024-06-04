f58cda58bd5f819274da4d191c908c867f36c29a
psa_code <p.sreyanshuspazz@gmail.com>
2024-06-04 08:06:48 +0530

    Context API

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React,{useContext,useState} from 'react'
+import UserContext from '../context/userContext'
+
+function Profile() {
+    const {user} = useContext(UserContext)
+
+    if(!user) return <div>Please Login</div>
+
+    return <div>Welcome : {user.username}</div>
+}
+
+export default Profile

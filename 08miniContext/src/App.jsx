0ea33294955007473cb76f9bbcc676314a3e3ad4
psa_code <p.sreyanshuspazz@gmail.com>
2024-06-08 07:49:55 +0530

    Context Over

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,8 +1,10 @@
 
+import { useContext } from 'react'
 import './App.css'
 import Login from './components/Login'
 import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
+import UserContext from './context/userContext'
 
 function App() {
 

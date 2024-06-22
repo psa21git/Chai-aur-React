8714e60de6aa3e85eb6ce00621b4c2309f43e62d
psa_code <p.sreyanshuspazz@gmail.com>
2024-06-22 12:07:57 +0530

    Components

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,6 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 export {
-    Header,Footer
+    Header,Footer,Container,Logo,LogoutBtn
 }

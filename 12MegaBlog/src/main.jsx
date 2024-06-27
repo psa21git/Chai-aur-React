35764155e93a49d338c32a4d6073b6509e73bcc2
psa_code <p.sreyanshuspazz@gmail.com>
2024-06-27 20:19:37 +0530

    Little changes

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -5,17 +5,17 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './Pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 
-import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost";
+import AddPost from "./Pages/AddPost";
+import Signup from './Pages/Signup'
+import EditPost from "./Pages/EditPost";
 
-import Post from "./pages/Post";
+import Post from "./Pages/Post";
 
-import AllPosts from "./pages/AllPosts";
+import AllPosts from "./Pages/AllPosts";
 
 const router = createBrowserRouter([
   {
--- 12MegaBlog/src/store/store.js
+++ 12MegaBlog/src/store/store.js
@@ -1,8 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
+import authSlice from './authSlice'
 
 export const store = configureStore({
-    reducer:{
-        
+    reducer: {
+        auth : authSlice,
+        //TODO: add more slices here for posts
     }
 })
 
+export default store

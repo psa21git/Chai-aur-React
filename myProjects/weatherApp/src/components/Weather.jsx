a7fa12f1e12efdec314cef5c93427d91ec4585f1
psa_code <p.sreyanshuspazz@gmail.com>
2024-07-12 08:32:43 +0530

    weather jsx

--- myProjects/weatherApp/src/components/Weather.jsx
+++ myProjects/weatherApp/src/components/Weather.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWeather from '../hooks/useWeather'
 import { setWeather } from '../features/weatherSlice'
 import {useSelector,useDispatch} from 'react-redux'
 
 function Weather() {
     const [isVisible,setIsVisible] = useState(false)
-
+    // const [api,setApi] = useState({})
     const [loc,setLoc] = useState('')
 
     const dispatch = useDispatch()
 
-    
-
     const location = useSelector(state => state.loc)
     const api = useWeather(location) ?? {}
+    // setApi(useWeather(location) ?? {})
+    // useEffect(()=>{
+    //     setApi(useWeather(location) ?? {})
+    // },[location])
     
     const toggleVisibility = ()=>{
         if(api!={}) {
@@ -24,17 +26,17 @@ function Weather() {
         }
     }
     
-    console.log(isVisible)
+    console.log(api, isVisible, location)
 
     const setWeatherHandler = (e) => {
         e.preventDefault()
         dispatch(setWeather(loc))
         toggleVisibility()
-        setLoc('')
+        setLoc('')    
     }
 
     const temp_celcius = api['temp_c'] ?? ' '
-    const condition = api['condition'] ?? {}
+    const condition = api['condition'] ?? " "
     const weatherText = condition['text'] ?? ' '
     const weatherIcon = condition['icon'] ?? ''
     
@@ -53,24 +55,24 @@ function Weather() {
                         className="block p-2.5 w-1/2 mx-5 h-10  text-sm text-gray-900 bg-gray-50 rounded-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                     />
                     <button type='submit'
-                    className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center me-2 mb-2absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'   
-                    // onClick={()=>toggleVisibility()}    
+                    className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center me-2 mb-2absolute top-0 end-0 h-full p-2.5 text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'      
                     >
                         <img src="https://cdn-icons-png.flaticon.com/128/10412/10412430.png" className='w-4 h-4' alt="" />
                     </button>
                 </div>
                 
             </form>
-            {isVisible && <div className='w-full text-white p-4 text-xl text-center bg-gray-700 '>
-                City : {location}
-                <br />
-                Temperature in celcius : {temp_celcius} *C
-                <br />
-                Weather : {weatherText}
-                <img src={weatherIcon} className='block mx-auto h-100 w-auto' />
-            </div>
-            }
+            {isVisible && 
+                <div className='w-full text-white p-4 text-xl text-center bg-gray-700 '>
+                    City : {location}
+                    <br />
+                    Temperature in celcius : {temp_celcius} *C
+                    <br />
+                    Weather : {weatherText}
+                    <img src={weatherIcon} className='block mx-auto h-100 w-auto' />
+                </div>
             
+            }
         </div>
         
     )
